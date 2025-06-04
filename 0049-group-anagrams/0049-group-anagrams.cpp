99379e9a88bb078a0e584e2ee40cc582157cd790
Chirag Yadav <162439081+chirag23177@users.noreply.github.com>
2025-06-04 17:52:19 +0530

    Time: 18 ms (54.22%), Space: 25 MB (67.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> groupMap;
-
-        for (string& s : strs) {
-            // Frequency count vector as key
-            vector<int> count(26, 0);
-            for (char c : s) count[c - 'a']++;
-
-            // Convert count vector to string as unique key
-            string key;
-            for (int freq : count) {
-                key += "#" + to_string(freq);  // use # as delimiter
-            }
-
-            groupMap[key].push_back(s);
-        }
-
+        unordered_map<string, vector<string>> hmap;
         vector<vector<string>> result;
-        for (auto& entry : groupMap) {
-            result.push_back(move(entry.second));
+        for(auto word:strs){
+            string sortedWord = word;
+            sort(sortedWord.begin(), sortedWord.end());
+            hmap[sortedWord].push_back(word);
         }
 
+        for(auto& [key,group] : hmap){
+            result.push_back(group);
+        }
         return result;
     }
 };
