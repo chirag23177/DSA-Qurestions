e7f52ef4a86ae5fa6c1222bdb7fc8f9dbf0bc5c7
Chirag Yadav <162439081+chirag23177@users.noreply.github.com>
2025-06-24 11:57:23 +0530

    Time: 3 ms (73.19%), Space: 10.9 MB (41.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.length();
-        if(n == 0) return 0;
+        if (n == 0)
+            return 0;
 
-        vector<int> freq(26,0);
+        vector<int> freq(26, 0);
 
-        int l = 0;
-        int r = 0;
-        int maxlength = 0;
-        int maxfreq = 0;
+        int l = 0,r = 0, maxlength = 0, maxfreq = 0;
 
-        while(r<n){
+        while (r < n) {
             freq[s[r] - 'A']++;
-            maxfreq = max(maxfreq,freq[s[r] - 'A']);
+            maxfreq = max(maxfreq, freq[s[r] - 'A']);
 
-            if((r-l+1) -maxfreq >k){
+            if ((r - l + 1) - maxfreq > k) {
                 freq[s[l] - 'A']--;
                 l++;
             }
-            if((r-l+1) -maxfreq <=k){
-                maxlength = max(maxlength,(r-l+1));
-                r++;
+            if ((r - l + 1) - maxfreq <= k) {
+                maxlength = max(maxlength, (r - l + 1));
             }
-
+            r++;
         }
 
         return maxlength;
