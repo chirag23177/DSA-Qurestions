8b0f0ce1fad9ebe2fa8c909e7d607f3d41be8db4
Chirag Yadav <162439081+chirag23177@users.noreply.github.com>
2025-06-24 11:54:18 +0530

    Time: 3 ms (73.19%), Space: 10.7 MB (91.84%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,48 +3,25 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.length();
 
-        unordered_map<char,int> freq;
+        vector<int> freq(26,0);
 
         int l = 0;
         int maxlength = 0;
         int maxfreq = 0;
 
         for(int r = 0; r<n; r++){
-            freq[s[r]]++;
-            maxfreq = max(maxfreq,freq[s[r]]);
+            freq[s[r] - 'A']++;
+            maxfreq = max(maxfreq,freq[s[r] - 'A']);
 
-            while((r-l+1) -maxfreq >k){
-                freq[s[l]]--;
+            if((r-l+1) -maxfreq >k){
+                freq[s[l] - 'A']--;
                 l++;
             }
+            if((r-l+1) -maxfreq <=k){
+                maxlength = max(maxlength,(r-l+1));
+            }
 
-            maxlength = max(maxlength,(r-l+1));
         }
-
-
-        // while(r<n){
-        //     if(freq.find(s[r]) == freq.end()){
-        //         freq.insert({s[r],1});
-        //     }
-        //     else{
-        //         freq[s[r]]++;
-        //     }
-
-        //     maxfreq = max(maxfreq, freq[s[r]]);
-
-        //     while( (r-l+1) - maxfreq > k){
-        //         freq[s[l]]--;
-        //         maxfreq = 0;
-        //         for(auto [key,val] : freq){
-        //             maxfreq =max(maxfreq,val);
-        //         }
-        //         l++;
-        //     }
-        //     if( (r-l+1) - maxfreq <= k){
-        //         maxlength = max(maxlength, (r-l+1));
-        //         r++;
-        //     }
-        // }
 
         return maxlength;
     }
