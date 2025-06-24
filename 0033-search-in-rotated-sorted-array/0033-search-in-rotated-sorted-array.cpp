71f5a2dfbb506bc4233df3ccd54ae359ed3a45c6
Chirag Yadav <162439081+chirag23177@users.noreply.github.com>
2025-06-24 18:04:01 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    int findElement(vector<int>& nums, int l, int r, int target){
-        if(l>r) return -1;
-        int mid = l+(r-l)/2;
-        if(nums[mid] == target){
-            return mid;
-        }
-        else if(target < nums[mid]){
-            return findElement(nums, l, mid-1, target);
-        }
-        else{
-            return findElement(nums, mid+1, r, target);
-        }
-    }
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
+        int l = 0, r = nums.size() - 1;
 
-        while(l<r){
-            int mid = l+(r-l)/2;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-            if(nums[mid]<nums[r]){
-                r = mid;
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half is sorted
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
             }
-            else{
-                l = mid+1;
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
         }
 
-        int minIdx = l;
-        int a = findElement(nums,0,l-1,target);
-        int b = findElement(nums,l,nums.size()-1,target);
-
-        if(a == -1 && b == -1){
-            return -1;
-        }
-        else{
-            return max(a,b);
-        }
+        return -1;
     }
 };
