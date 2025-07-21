0f90625eb7852dce0f8ccd5532500213acace0e2
Chirag Yadav <162439081+chirag23177@users.noreply.github.com>
2025-07-21 11:28:53 +0530

    Time: 2 ms (56.75%), Space: 10 MB (11.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        unordered_map<char,int> s_set;
-        for(char a : s){
-            s_set[a]++;
-        }
-        for(char b : t){
-            s_set[b]--;
-            if(s_set[b]<0) 
-            return false;
+        unordered_map<char, int> s_map;
+        unordered_map<char, int> t_map;
+
+        for(auto ch : s){
+            s_map[ch]++;
         }
 
-        return true;;
+        for(auto ch2 : t){
+            t_map[ch2]++;
+        }
+
+        return s_map == t_map;
     }
 };
