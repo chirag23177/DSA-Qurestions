917d37725db1ee127cc0e86dbe10e97de0cbc37c
Chirag Yadav <162439081+chirag23177@users.noreply.github.com>
2025-07-21 11:38:25 +0530

    Time: 13 ms (80.42%), Space: 26.1 MB (37.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> hmap;
-        vector<vector<string>> result;
-        for(auto word:strs){
-            string sortedWord = word;
-            sort(sortedWord.begin(), sortedWord.end());
-            hmap[sortedWord].push_back(word);
+        vector<vector<string>> res;
+
+        unordered_map<string, vector<string>> anagrams;
+
+        for(auto word : strs){
+            string sorted_word = word;
+            sort(sorted_word.begin(), sorted_word.end());
+
+            anagrams[sorted_word].push_back(word);
         }
 
-        for(auto& [key,group] : hmap){
-            result.push_back(group);
+        for(auto [base, words]: anagrams){
+            res.push_back(words);
         }
-        return result;
+
+        return res;
     }
 };
